da5b6f3d7445465205df0154a1b512053efd5140
Jacob <jbale444@outlook.com>
2023-12-17 18:43:51 +0000

    Started creating graphQL frontend application building on provided seed to create a view for authors and began looking at a view for books for exercises 8.9 & 8.10

---
+++ library-frontend/src/components/Books.js
@@ -0,0 +1,45 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+query {
+  allBooks {
+    author,
+    genres,
+    published,
+    title
+  } 
+}`
+
+const Books = (props) => {
+  if (!props.show) {
+    return null
+  }
+
+  const books = useQuery(ALL_BOOKS).data.allBooks
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {console.log(books)}
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
