82686dad66364af0ae44c980114660589fed887e
Jacob <jbale444@outlook.com>
2024-01-10 08:45:14 +0000

    Added functionality so that buttons are added for each genre allowing the user to filter the books table by genre for exercise 8.19

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client'
+import { useState } from 'react'
 
 const ALL_BOOKS = gql`
 query {
@@ -14,7 +15,18 @@ query {
 
 const Books = () => {
 
+  const [filter, setFilter] = useState('')
+  const genres = new Set([])
   const books = useQuery(ALL_BOOKS)
+
+  const filterBooks = (event) => {
+    event.preventDefault()
+    if (event.target.id === 'all') {
+      setFilter('')
+    } else {
+      setFilter(event.target.id)
+    }
+  }
 
   if (books.loading) {
     return (<div>loading...</div>)
@@ -31,15 +43,27 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books.data.allBooks.map((a) => {
+            if (filter === '' || a.genres.includes(filter)) {
+              return <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            }
+          })}
         </tbody>
       </table>
+      <br></br>
+      {books.data.allBooks.map((a) => (
+        a.genres.map(g => {
+          if (!genres.has(g)) {
+            genres.add(g)
+            return <button key={g} id={g} onClick={filterBooks}>{g}</button>
+          }
+        })
+      ))}
+      <button id={'all'} onClick={filterBooks}>All Genres</button>
     </div>
   )
 }
